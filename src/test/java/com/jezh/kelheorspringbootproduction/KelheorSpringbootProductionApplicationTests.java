83a269b826dd2ba003113add3d48e7b2c77da721
jezhische <jezhische@gmail.com>
2017-07-17 09:37:23 +0300

    first commit

---
+++ src/test/java/com/jezh/kelheorspringbootproduction/KelheorSpringbootProductionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jezh.kelheorspringbootproduction;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KelheorSpringbootProductionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
